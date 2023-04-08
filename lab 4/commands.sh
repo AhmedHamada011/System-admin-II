604c24c556790927e9b398746c681769a8702138
Ahmed Hamada <ahmedhamada015@gmail.com>
2023-04-08 16:53:03 +0200

    add shebang to commands file

--- lab 4/commands.sh
+++ lab 4/commands.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #Using apt install apache2 server
 sudo apt install apache2
 
@@ -10,14 +11,14 @@ sudo echo '<html>
 sudo apt install libapache2-mod-php8.1
 
 #create new Named VirtulHost to serve new php project with special access log 
-echo '<VirtualHost *:80>
-    ServerName myProject.local
-    DocumentRoot /var/www/html/myProject
-	ServerAdmin webmaster@localhost
-	DocumentRoot "/var/www/html/myProject"
-	ErrorLog ${APACHE_LOG_DIR}/error-myProject.log
-	CustomLog ${APACHE_LOG_DIR}/access-myProject.log combined
-</VirtualHost>' > /etc/apache2/sites-available/myProject.conf
+sudo echo '<VirtualHost *:80>
+        ServerName myProject.local
+        DocumentRoot /var/www/html/myProject
+	    ServerAdmin webmaster@localhost
+	    DocumentRoot "/var/www/html/myProject"
+	    ErrorLog ${APACHE_LOG_DIR}/error-myProject.log
+	    CustomLog ${APACHE_LOG_DIR}/access-myProject.log combined
+    </VirtualHost>' > /etc/apache2/sites-available/myProject.conf
 
 #create simple index.php file for the php site
 echo '<?php
